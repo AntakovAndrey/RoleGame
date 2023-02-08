69daa079bbf8ed750a6d76e81b2c2311831ebd64
adski13 <71898448+adski13@users.noreply.github.com>
2023-02-08 21:52:43 +0300

    IMagic

---
+++ RoleGame/RoleGame/Magic/IMagic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoleGame.Magic
+{
+    internal interface IMagic
+    {
+        void PerformMagic();
+        void PerformMagic(Character aimCharacter);
+        void PerformMagic(double impactForce);
+        void PerformMagic(Character aimCharacter, double impactForce);
+    }
+}
