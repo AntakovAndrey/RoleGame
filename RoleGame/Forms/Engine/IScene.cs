306c05eeca4d26e3337a0fbccb30349af2932c80
adski13 <71898448+adski13@users.noreply.github.com>
2023-02-20 12:45:24 +0300

    IScene

---
+++ RoleGame/Forms/Engine/IScene.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forms.Engine
+{
+    internal interface IScene
+    {
+        void DrawBack(Graphics g, int x, int y);
+        void DrawObjects(Graphics g);
+    }
+}
