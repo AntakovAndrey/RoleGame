31e3a3c85e838880a5d3fb8ad6a680cb635fe391
adski13 <71898448+adski13@users.noreply.github.com>
2023-02-20 11:58:56 +0300

    Vector

---
+++ RoleGame/Forms/Engine/Vector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forms.Engine
+{
+    internal class Vector
+    {
+        int x;
+        int y;
+        public Vector() //Инициализация
+        {
+            this.x = 0;
+            this.y = 0;
+        }
+        public Vector(int x, int y) //Перегрузка инициализации
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public int X => x; //Получаем координаты
+        public int Y => y;
+
+        public static Vector operator +(Vector a, Vector b) => new Vector(a.x + b.x, a.y + b.y);
+        public static Vector operator -(Vector a, Vector b) => new Vector(a.x - b.x, a.y - b.y);
+    }
+}
