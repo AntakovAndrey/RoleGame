20039a3bcf537e69362aff452880d0e6c25c2e61
Vlados <Vladsh407@gmail.com>
2023-02-27 19:07:43 +0300

    inv1

---
+++ RoleGame/Forms/Game/Inventory.cs
@@ -0,0 +1,33 @@
+﻿using Forms.Game.Magic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoleGame
+{
+    public class Inventory
+    {
+        protected List<InventoryCell> inventory;
+        public List<InventoryCell> Inventory1
+        { get => inventory; set => inventory = value; }
+        public Inventory(List<InventoryCell> inventory)
+        {
+            this.inventory = inventory;
+        }
+        public void AddArtefact(InventoryCell artefact)
+        {
+            inventory.Add(artefact);
+        }
+        public void ToThrowOutArtefact(InventoryCell artefact)
+        {
+            inventory.Remove(artefact);
+        }
+        public void ToPassArtefact(Character character,InventoryCell artefact)
+        {
+            character.Inventory.AddArtefact(artefact);
+            inventory.Remove(artefact);
+        }
+    }
+}
---
+++ RoleGame/Forms/Game/Magic/InventoryCell.cs
@@ -0,0 +1,20 @@
+﻿using RoleGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forms.Game.Magic
+{
+    public class InventoryCell
+    {
+        protected Artefact cell;
+        public Artefact Cell
+        { get => cell;set => cell = value; }
+        public InventoryCell(Artefact cell)
+        {
+            this.cell = cell;
+        }
+    }
+}
